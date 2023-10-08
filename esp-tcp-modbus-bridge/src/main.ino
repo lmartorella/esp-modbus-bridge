cced0c8dc05033d07e8ba46c12f8cacec9933e66
Luciano Martorella <mad_lux_it@yahoo.it>
2023-10-08 14:47:00 +0200

    - Fixed splitted messages

--- esp-tcp-modbus-bridge/src/main.ino
+++ esp-tcp-modbus-bridge/src/main.ino
@@ -172,6 +172,10 @@ void setup() {
 #endif
   rtu.master();
   rtu.onRaw(cbRtuRaw, true);
+  // Sofar doesn't follow the modbus spec, and it is splitting messages with more than 3.5 * space time sometimes
+  // ((1 / 9600) * 11) * 3.5 = 4ms
+  // Use 8ms instead
+  rtu.setInterFrameTime(8000);
 
   TelnetStream.begin();
 
